d94b41f2eee8f22b0ecb7e185a74bc3bd2be9c21
brian-nyabuto12 <brian.nyabuto@student.moringaschool.com>
2021-05-15 11:14:21 +0300

    add an array for the date and time calculation

--- src/app/date-pipe.pipe.ts
+++ src/app/date-pipe.pipe.ts
@@ -1,3 +1,4 @@
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
@@ -5,8 +6,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DatePipePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+
+  transform(value :any): number {
+    let today:Date = new Date();
+    let todayWithNotime:any = new Date(today.getFullYear(),today.getMonth())
+    var dateDifference =Math.abs(todayWithNotime - value)
+    const secondsInAday= 86400;
+
+    var dateDifferenceSeconds=dateDifference*0.001;
+    var dateCounter = dateDifferenceSeconds/secondsInAday;
+     if(dateCounter >= 1 && todayWithNotime > value){
+       return dateCounter;
+     }
+    else{
+      return 0;
+    }
+
+  
   }
 
 }
